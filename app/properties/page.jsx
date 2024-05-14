b9fa0f2f1fd9bd39b2be4f16c44b76c45eaffd56
mborzechowsk <m.borzechowski@gmail.com>
2024-05-14 21:40:55 +0200

    14.05

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,5 @@
-import properties from '@/properties.json'
-import PropertyCard from '@/components/PropertyCard'
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
 
 const PropertiesPage = () => {
   return (
@@ -7,16 +7,16 @@ const PropertiesPage = () => {
       <div className="container-xl lg:container m-auto px-4 py-6">
         {properties.lenght === 0 ? (
           <p>No properties found</p>
-        ) : (<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {properties.map((property) => (
-          <PropertyCard key={ property._id} property={property} />
-        ))}
-      </div>) }
-        
-        </div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
     </section>
+  );
+};
 
-  )
-}
-
-export default PropertiesPage
+export default PropertiesPage;
