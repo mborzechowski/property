28778de082be4956163f195de60972a169abc7be
mborzechowsk <m.borzechowski@gmail.com>
2024-06-29 23:40:45 +0200

    adding bokmarked logic

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -13,7 +13,7 @@ export const POST = async (request) => {
 
         const sessionUser = await getSessionUser();
 
-        if (!session || !session.userId) {
+        if (!sessionUser || !sessionUser.userId) {
             return new Response('User Id is required', { status: 401 })
         }
 
