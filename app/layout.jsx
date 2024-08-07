e8336385befeb77b854667202a93c176746e993f
mborzechowsk <m.borzechowski@gmail.com>
2024-08-07 23:55:18 +0200

    Global Contex for unread messages

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,29 +1,32 @@
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import "@/assets/styles/globals.css";
-import AuthProvider from "@/components/AuthProvider";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import '@/assets/styles/globals.css';
+import AuthProvider from '@/components/AuthProvider';
+import { ToastContainer } from 'react-toastify';
+import { GlobalProvider } from '@/contex/GlobalContex';
+import 'react-toastify/dist/ReactToastify.css';
 export const metadata = {
-  title: "ProportyPulse | Find The Perfect Rental",
-  description: "Find The Perfect Rental",
-  keywords: "rental, find rentals, find properties",
+  title: 'ProportyPulse | Find The Perfect Rental',
+  description: 'Find The Perfect Rental',
+  keywords: 'rental, find rentals, find properties',
 };
 
 const MainLayout = ({ children }) => {
   return (
-    <AuthProvider>
-      <html lang="en">
-        <body>
-          <main>
-            <Navbar />
-            {children}
-          </main>
-          <Footer />
-          <ToastContainer />
-        </body>
-      </html>
-    </AuthProvider>
+    <GlobalProvider>
+      <AuthProvider>
+        <html lang='en'>
+          <body>
+            <main>
+              <Navbar />
+              {children}
+            </main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </AuthProvider>
+    </GlobalProvider>
   );
 };
 
